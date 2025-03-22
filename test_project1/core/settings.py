8b611046319207f98c8fbdd55dc6f8f091f14d59
Vladimir Penzin <pvenv@icloud.com>
2025-03-22 14:45:07 -0400

    Ап

--- test_project1/core/settings.py
+++ test_project1/core/settings.py
@@ -1,7 +1,9 @@
 import pathlib
 
 # Базовая директория проекта
-BASE_DIR = pathlib.Path(__file__).resolve().parent
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+print('BASE_DIR', BASE_DIR)
 
 # Настройки базы данных
 DATABASES = {
