ee7bd1734a32615f2973a661f5b9418a7ed547af
Vladimir Penzin <pvenv@icloud.com>
2025-03-23 17:13:10 -0400

    Ап

--- test_project1/apps/admin/urls.py
+++ test_project1/apps/admin/urls.py
@@ -12,18 +12,29 @@ def url_for(endpoint, **kwargs):
     Функция для генерации URL на основе endpoint и дополнительных параметров.
     В данном случае endpoint игнорируется, так как мы используем только filename.
     """
-    if endpoint == "static":
-        filename = kwargs.get("filename")
-        if filename:
-            return f"static/{filename}"
-    return ""
+    if not kwargs:
+        return f"/{endpoint}"
+    
+    path = f"/{endpoint}"
+    for key, value in kwargs.items():
+        path += f"/{value}"
+    
+    return path
 
 
 @router.get("/", response_model=None)
 async def test(request: Request):    
     return render(request=request, template_name="pages/index.html", context={
         "url_for": url_for,
-        "parent": "/",
+        "parent": "home",
+        "segment": "test"
+    })
+
+@router.get("/accounts_login", response_model=None)
+async def test(request: Request):    
+    return render(request=request, template_name="accounts/login.html", context={
+        "url_for": url_for,
+        "parent": "home",
         "segment": "test"
     })
 
