690548c1636d8e9dfbeb0a82140784c8c9fef9ee
Vladimir Penzin <pvenv@icloud.com>
2025-03-23 16:39:24 -0400

    Ап

--- test_project1/apps/admin/urls.py
+++ test_project1/apps/admin/urls.py
@@ -4,16 +4,30 @@ from cotlette.shortcuts import render
 router = APIRouter()
 
 import os
-def url_for(filename, **kwargs):
-    print('path', filename)
-    return
+
+
+
+def url_for(endpoint, **kwargs):
+    """
+    Функция для генерации URL на основе endpoint и дополнительных параметров.
+    В данном случае endpoint игнорируется, так как мы используем только filename.
+    """
+    if endpoint == "static":
+        filename = kwargs.get("filename")
+        if filename:
+            return f"static/{filename}"
+    return ""
+
+
 
 @router.get("/test", response_model=None)
 async def test(request: Request):    
-    return render(request=request, template_name="pages/index.html", context={
+    return render(request=request, template_name="pages/billing.html", context={
         "config": {
             "ASSETS_ROOT": "static/assets",
             "APP_NAME": "Cotlette"
         },
-        "url_for": url_for
+        "url_for": url_for,
+        "parent": "/",
+        "segment": "test"
     })
