b818c7048bc8d96ccbb8a5ef8bffdfa12a71825a
Vladimir Penzin <pvenv@icloud.com>
2025-03-25 17:35:18 -0400

    Ап

--- example/apps/home/__init__.py
+++ example/apps/home/__init__.py
@@ -4,5 +4,5 @@ from .api import router as api_router
 from .urls import router as urls_router
 
 router = APIRouter()
-router.include_router(urls_router)
-router.include_router(api_router, prefix="/api", tags=["common"],)
+router.include_router(urls_router, include_in_schema=False)
+router.include_router(api_router, prefix="/api", tags=[])
--- src/cotlette/apps/admin/__init__.py
+++ src/cotlette/apps/admin/__init__.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from .urls import router as urls_router
 
 
-router = APIRouter(include_in_schema=False)
-router.include_router(urls_router)
+router = APIRouter()
+router.include_router(urls_router, include_in_schema=False)
