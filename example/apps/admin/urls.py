7bae7c1ff04ebc0f2106e66e39fa2bf3841021d8
Vladimir Penzin <pvenv@icloud.com>
2025-03-30 16:39:57 -0400

    Ап

--- example/apps/admin/urls.py
+++ example/apps/admin/urls.py
@@ -23,14 +23,91 @@ def url_for(endpoint, **kwargs):
     return path
 
 
+# @router.get("/", response_model=None)
+# async def test(request: Request):    
+#     return render_template(request=request, template_name="pages/index.html", context={
+#         "url_for": url_for,
+#         "parent": "home1",
+#         "segment": "test",
+#         "config": request.app.settings,
+#     })
+
+
+# TODO
+
+
+
+from fastapi import Depends, HTTPException, status
+from jose import jwt, JWTError
+
+from config.settings import SECRET_KEY
+ALGORITHM = "HS256"
+
+from fastapi.security import OAuth2PasswordBearer
+# Создаем схему для извлечения токена из заголовка Authorization
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    """
+    Проверяет токен и извлекает email пользователя.
+    """
+    print('token', token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Декодируем токен
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return email
+
+# @router.get("/", response_model=None)
+# async def test(
+#     request: Request,
+#     current_user: str = Depends(get_current_user)  # Проверяем токен
+# ):
+#     """
+#     Защищенный эндпоинт, доступный только авторизованным пользователям.
+#     """
+#     print('request', request)
+#     return render_template(
+#         request=request,
+#         # template_name="pages/index.html",
+#         template_name="pages/protected.html",
+#         context={
+#             "url_for": url_for,
+#             "parent": "home1",
+#             "segment": "test",
+#             "config": request.app.settings,
+#             "user_email": current_user,  # Передаем email пользователя в контекст
+#         },
+#     )
+
 @router.get("/", response_model=None)
 async def test(request: Request):    
-    return render_template(request=request, template_name="pages/index.html", context={
+    return render_template(request=request, template_name="pages/protected.html", context={
         "url_for": url_for,
         "parent": "home1",
         "segment": "test",
         "config": request.app.settings,
     })
+
+
+# Защищенный эндпоинт
+@router.get("/protected")
+async def protected_endpoint(current_user: str = Depends(get_current_user)):
+    return {"message": f"Hello, {current_user}"}
+
+
+
+
+
 
 @router.get("/accounts_login", response_model=None)
 async def test(request: Request):    
