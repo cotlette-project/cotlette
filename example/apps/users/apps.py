80b173975d5a325c349e3f4d70af724fece3d453
Vladimir Penzin <pvenv@icloud.com>
2025-03-30 16:04:08 -0400

    Ап

--- example/apps/users/apps.py
+++ example/apps/users/apps.py
@@ -3,4 +3,4 @@ from cotlette.apps import AppConfig
 
 class AdminConfig(AppConfig):
     default_auto_field = 'cotlette.db.models.BigAutoField'
-    name = 'cotlette.apps.users'
+    name = 'apps.users'
