64550f6ae612033fb26025f28d54bb9d39df4456
Vladimir Penzin <pvenv@icloud.com>
2025-03-31 13:22:31 -0400

    Ап

--- example/core/__init__.py
+++ example/core/__init__.py
@@ -10,13 +10,41 @@ from fastapi.responses import JSONResponse
 from jose import JWTError, jwt
 from config.settings import SECRET_KEY, ALGORITHM
 
+from cotlette.shortcuts import render_template
+
+
+
+
+def url_for(endpoint, **kwargs):
+    """
+    Функция для генерации URL на основе endpoint и дополнительных параметров.
+    В данном случае endpoint игнорируется, так как мы используем только filename.
+    """
+    if not kwargs:
+        return f"/{endpoint}"
+    
+    path = f"/{endpoint}"
+    for key, value in kwargs.items():
+        path += f"/{value}"
+    
+    return path
+
+
+
+
 @app.middleware("http")
 async def auth_middleware(request: Request, call_next):
     # Проверяем, относится ли запрос к /admin/*
     if request.url.path.startswith("/admin"):
         token = request.cookies.get("access_token")  # Ищем токен в cookie
         if not token:
-            return JSONResponse(status_code=401, content={"detail": "Missing token"})
+            # return JSONResponse(status_code=401, content={"detail": "Missing token"})
+            return render_template(request=request, template_name="401.html", context={
+                "url_for": url_for,
+                # "parent": "/",
+                # "segment": "test",
+                "config": request.app.settings,
+            })
         
         try:
             # Декодируем токен
